e13c737f00bf3f9dd167a3c6bd6cc8a46eeaa2bf
chinmayraii <chinmayrai75@gmail.com>
2023-05-19 13:58:48 +0530

    'update'

--- mortgages/app/urls.py
+++ mortgages/app/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('',views.index),
-    path('mortgage_chatbot', views.mortgage_chatbot, name='mortgage_chatbot'),
+    path('chatbot', views.chatbot, name='chatbot'),
 ]
